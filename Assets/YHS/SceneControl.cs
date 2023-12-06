a7e9cd7fbdbaa9ad32c683d0311cf8606b01d9f9
HyeonSikYun <109672921+HyeonSikYun@users.noreply.github.com>
2023-12-07 00:32:54 +0900

    dafadf
    dafadf

---
+++ Assets/YHS/SceneControl.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LSD_Scene()
+    {
+        SceneManager.LoadScene("SceneTest");
+    }
+
+    public void Heroin_Scene()
+    {
+        SceneManager.LoadScene("SceneTest");
+    }
+
+    public void Cocaine_Scene()
+    {
+        SceneManager.LoadScene("SceneTest");
+    }
+}
