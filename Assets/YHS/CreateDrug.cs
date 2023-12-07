e8ce0ec56ee199360f5fcad03fe297bd84d06f59
HyeonSikYun <109672921+HyeonSikYun@users.noreply.github.com>
2023-12-08 00:50:40 +0900

    dfdf
    dfdf

---
+++ Assets/YHS/CreateDrug.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateDrug : MonoBehaviour
+{
+    public GameObject cannabis;
+
+    public void cannabis_Create()
+    {
+        cannabis.SetActive(true);
+    }
+}
