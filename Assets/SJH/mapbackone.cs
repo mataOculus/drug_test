4bd11b13f346827f8b4aceec7cc6433d615ebfd0
smallmoon2 <106749904+smallmoon2@users.noreply.github.com>
2023-12-10 03:31:32 +0900

    1234
    1234

--- Assets/SJH/mapbackone.cs
+++ Assets/SJH/mapbackone.cs
@@ -2,23 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class mainbacktime : MonoBehaviour
+public class mapbackone : MonoBehaviour
 {
+    // Start is called before the first frame update
     public float time = 30f;
     public float map = 1f;
-
+    public CameraEffect CameraEffect;
     void Update()
     {
-
+        if (time < 6)
+        {
+            CameraEffect.enabled = true;
+        }
 
         if (time > 0)
         {
-            time -= Time.deltaTime; 
+            time -= Time.deltaTime;
         }
         else
         {
-            LoadMainScene(); 
+            LoadMainScene();
         }
     }
 
